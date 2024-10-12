62707228dcb59ff39a9ed3c5976171dea2537daf
ZackSid07 <algalib908@gmail.com>
2024-10-12 13:39:50 +0600

    first commit

---
+++ Js/main.js
@@ -0,0 +1,131 @@
+// Document.write("My name is abdullah Al Glaib");
+// console.log("hdbvjhdj hvd")
+// alert("i'm ok")
+
+
+/**
+ * todo : voting application
+ * todo : even or odd checking
+ */
+
+
+// if(true){
+//      console.log("this is true");
+// }
+// else{
+//     console.log("this is false");
+// }
+
+// let x = 10 > 50 ;
+// let y = 20 < 40
+
+// if(x){
+//     console.log("true: 10 > 50");
+    
+// }
+// else if(y){
+//     console.log("True: 20 < 40");
+// }
+// else{
+//     console.log("False: 10 < 50");
+// }
+
+
+
+/**
+ * todo : Voting conditions
+ */
+
+// const userInput = prompt("please enter the age : ") //user input
+// // console.log(+userInput);
+// const originalInput = +userInput;
+
+// if(originalInput > 100 && originalInput < 400){
+//     console.log("You are over-aged");
+// }
+// else if(originalInput > 0 && originalInput < 18){
+//     console.log("You are Under-aged");
+// }
+// else if(originalInput >= 18 && originalInput <= 100){
+//     console.log("You are elegible for the vote");
+// }
+// else{
+//     console.log("I don't know");
+    
+// }
+
+/**
+ * todo : Check even or odd
+ */
+
+// const userInput = prompt("please enter the Number : ") //user input
+// // console.log(+userInput);
+// const originalInput = +userInput;
+
+// if(originalInput % 2 == 0){
+//     console.log("even number");
+// }
+
+// else{
+//     console.log("Odd number");
+// }
+
+
+/**
+ * todo : take two user input and check which number is bigger 
+ */
+
+
+// const firstnumber = prompt("please enter the first number : ")
+// const first = +firstnumber;
+
+// const Secondnumber = prompt("please enter the Second number : ")
+// const Second = +Secondnumber;
+
+// if(first > Second){
+//     console.log("First number is bigger");
+    
+// }
+// else if(first < Second){
+//     console.log("Second number is bigger");
+// }
+// else{
+//     console.log("number equal");
+// }
+
+
+/**
+ * todo : Switch case
+ */
+
+const btn = prompt("Press a button : ")
+
+switch (btn) {
+    case "light":{
+        console.log("Light on");
+        break;
+    }
+
+    case "fan":{
+        console.log("Fan on");
+        break;
+    }
+
+    case "computer":{
+        console.log("Computer on");
+        break;
+    }
+    case "tv":{
+        console.log("TV on");
+        break;
+    }
+        
+        
+
+    default:
+        console.log("Connection error");
+        
+}
+
+  
+
